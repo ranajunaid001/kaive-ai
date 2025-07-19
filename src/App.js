227e7e4ec89d5ab8a1772744e9692af5f6e2e146
ranajunaid001 <130623056+ranajunaid001@users.noreply.github.com>
2025-07-19 06:43:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AdminPage from './pages/AdminPage';
+import Login from './pages/Login';
+import './App.css';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
-  
+
   return (
-    <div className="app">
-      {currentPage === 'home' ? (
-        <HomePage onNavigate={setCurrentPage} />
-      ) : (
-        <AdminPage onNavigate={setCurrentPage} />
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage onNavigate={setCurrentPage} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<AdminPage onNavigate={setCurrentPage} />} />
+          {/* Add more routes as needed */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
