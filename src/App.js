93cb1bf256384a96f0d4713f31ecfe922cc8c661
ranajunaid001 <130623056+ranajunaid001@users.noreply.github.com>
2025-07-19 08:53:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import Login from './pages/Login';
 import AppPage from './pages/AppPage';
 
 function App() {
+  // Check if we're on the app subdomain
+  const isAppSubdomain = window.location.hostname === 'app.kaive.xyz';
+  
+  // If on app subdomain, show the app page
+  if (isAppSubdomain) {
+    return (
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<AppPage />} />
+            <Route path="*" element={<AppPage />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+  
+  // Otherwise, show the normal routes
   return (
     <Router>
       <div className="app">
